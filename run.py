927245da72df68e392e2d9c53ebea600a97d864c
alhafikoibour <alhafikoiboiur@gmail.com>
2026-05-28 17:13:58 +0100

    initialisation de la base de donnée

--- run.py
+++ run.py
@@ -1,7 +1,11 @@
-from app import create_app
+from app import create_app, db
 
 app = create_app()
+# Initialisation de la base de données
+with app.app_context():
+    db.create_all()
+    print("Tables créées avec succès !")
 
 if __name__ == '__main__':
     print("[START] Demarrage du serveur StudSecure sur https://127.0.0.1:5000")
-    app.run(host='127.0.0.1', port=5000, ssl_context='adhoc', debug=True)
+    app.run(host='0.0.0.0.', port=5000, ssl_context='adhoc', debug=True)
